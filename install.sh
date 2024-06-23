06a9ff83c95fb5fcac7ae24062e3fd8e3fd8f573
Hynek Zamazal <heineken@volny.cz>
2024-06-23 22:54:49 +0200

    Removed ranger

--- install.sh
+++ install.sh
@@ -11,28 +11,12 @@ cd $MY_PATH
 #fetch updates for the setup
 git pull
 
-#initialize submodules
-echo "Downloading git submodules"
-git submodule update --init --recursive
-
-
 #update and upgrade current packages
 echo "Updating and Upgrading the system"
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
-#install essentials
-toilet "Installing essentials"
-sudo apt-get -y install build-essential make cmake cmake-curses-gui ccache pkg-config automake autoconf libc++-dev clang clang-format libffi-dev
-
-#install tools
-sudo apt-get -y install htop git unzip
-
 #install tmux
 echo "Installing tmux"
 bash $APPCONFIG_PATH/tmux/install.sh
-
-#install ranger
-toilet "Installing ranger"
-bash $APPCONFIG_PATH/ranger/install.sh
 
